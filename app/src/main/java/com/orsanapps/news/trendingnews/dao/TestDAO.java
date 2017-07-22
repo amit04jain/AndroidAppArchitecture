5e978632b3ed67ce6ec8632a3a80230a6d36bfb5
orsan <orsan.tech@gmail.com>
2017-07-22 23:04:02 +0530

    InitialCommit

--- app/src/main/java/com/orsanapps/news/trendingnews/dao/TestDAO.java
+++ app/src/main/java/com/orsanapps/news/trendingnews/dao/TestDAO.java
@@ -9,10 +9,20 @@ import com.orsanapps.news.trendingnews.sql.AbstractDatabaseManager;
 import com.orsanapps.news.trendingnews.sql.DatabaseHelper;
 import com.orsanapps.news.trendingnews.vo.TestVO;
 
-/**
- * Created by Home on 1/26/2017.
- */
+/* This file is part of Android App Architecture.
 
+    Android App Architecture is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.*/
 public class TestDAO extends AbstractDatabaseManager<TestVO> {
     private static final String TAG = "TestDAO";
     protected SQLiteDatabase database;
